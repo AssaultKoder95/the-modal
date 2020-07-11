a6b88d9e8477de7da78b3ccf8a5f82064d127454
manojkumardesai <manojkumardbe@gmail.com>
2020-07-11 15:29:04 +0530

    basic working modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Modal from './Modal';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Modal>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        </Modal>
       </header>
     </div>
   );
