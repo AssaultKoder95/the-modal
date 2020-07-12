e878ae320318e3fb957337449db24b6ac11ed8e5
manojkumardesai <manojkumardbe@gmail.com>
2020-07-12 11:44:24 +0530

    toggle modal feature

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Modal from './Modal';
+import { useState } from 'react';
 
 function App() {
+  const [showModal, setModalState] = useState(false);
+
+  const onToggle = () => {
+    setModalState(!showModal);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <Modal>
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        </Modal>
+        <button onClick={onToggle}>
+          Show logo in Modal
+        </button>
+        { showModal ? <Modal>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Logo in modal</p>
+          <button onClick={onToggle}>Close Modal</button>
+        </Modal> : null}
       </header>
     </div>
   );
